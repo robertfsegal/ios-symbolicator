0c1ccd33f28d804516a814a9a8cd56dc1ba36523
Robert Segal <rob@getsetgames.com>
2015-02-20 16:05:56 -0500

    - architecture is pulled out from crash file - base address is pulled out from crash file

--- ios-symbolicator.sh
+++ ios-symbolicator.sh
@@ -2,10 +2,21 @@
 
 
 BUNDLE="$1"
-ARCH="$2"
-BASE_ADDRESS="$3"
-FUNC_ADDRESS="$4"
+CRASH_FILE="$2"
+FUNC_ADDRESS="$3"
+
+line_number=$(grep -n "Binary Images" ${CRASH_FILE} | tr "Binary Images" " " | tr ":" " ")
+line_number=$((${line_number// }+1))
+
+ARCH=$(head -n ${line_number} ${CRASH_FILE} | tail -n 1 | tr " " "\n" | grep "arm")
+BASE_ADDRESS=$(head -n ${line_number} ${CRASH_FILE} | tail -n 1 | tr " " "\n" | head -n 1)
+
+echo "Architecture detected as - $ARCH"
+echo "Base address detected as - $BASE_ADDRESS"
+echo "Looking for function at address $FUNC_ADDRESS..."
 
 xcrun atos -o "$BUNDLE.app"/"$BUNDLE" -arch $ARCH -l $BASE_ADDRESS $FUNC_ADDRESS
+
+echo "Done."
 
 
